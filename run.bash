99405f168f4645271284010f56510c66b4883c68
Paul Tarvydas <guitarvydas@gmail.com>
2021-09-24 13:49:32 -0400

    add test4

--- run.bash
+++ run.bash
@@ -3,4 +3,4 @@
 ./pfr test2.txt test2.ohm test2.action
 ./pfr test2.txt test2.ohm test2a.action
 ./pf  test3.txt test3.ohm
-#./pfr test4.txt test4.ohm test4.action
+./pfr test3.txt test4.ohm test4.action
