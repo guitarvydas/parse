317c12a78e07ad0fdfb6a6077a3a0cb4f0121aa9
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-11 16:54:39 -0500

    make.bash creates pfr and pf as docker images

--- make.bash
+++ make.bash
@@ -1,4 +1,12 @@
 clear
 set -x
+
+rm -f Dockerfile
+cp Dockerfile-pfr Dockerfile
 docker build . --tag pfr
 docker run pfr 'test.txt test.ohm test.action'
+
+rm -f Dockerfile
+cp Dockerfile-pf Dockerfile
+docker build . --tag pf
+docker run pf 'test.txt test.ohm'
