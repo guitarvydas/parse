bc32b752a350bbfaa83cd2616f784ff46dbda3f2
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-17 08:01:08 -0500

    build dpfrs for docker

--- run.bash
+++ run.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
-./pfr test.txt test.ohm test.action
-./pfr test2.txt test2.ohm test2.action
-./pfr test2.txt test2.ohm test2a.action
-./pf  test3.txt test3.ohm
-./pfr test3.txt test4.ohm test4.action
+# ./pfr test.txt test.ohm test.action
+# ./pfr test2.txt test2.ohm test2.action
+# ./pfr test2.txt test2.ohm test2a.action
+# ./pf  test3.txt test3.ohm
+# ./pfr test3.txt test4.ohm test4.action
+
+./dpfrs test.txt test.ohm test.action support.js
+
+
